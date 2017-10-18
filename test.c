d5a08e4968cd2f24e88abb0e556e6f428fad56f8
George Le <george_le@student.uml.edu>
2017-10-18 02:04:15 +0000

    Finished eval_equation GL

--- test.c
+++ test.c
@@ -8,19 +8,78 @@
 
 typedef enum Bool { UNSET = -1, FALSE, TRUE} bool;
 
+void receives_input();
+double eval_equation(double left, char operation, double right, int repetition);
 bool askContinue();
 void clearKeyboardBuffer();
 
 int main(void)
 {
-    bool wantContinue = TRUE;
-    
-    
-    
     do
     {
-        // start from here next time
-    } while(wantContinue);
+        receives_input();
+    } while(askContinue());
+}
+
+void receives_input()
+{
+    // receives two values from the user to solve in an equation
+    int left, right;
+    // receives the return value of scanf to validate input
+    int number_of_conversions;
+    // receives the user's desired operation
+    char operation;
+    
+    printf("Enter in an equation for this program to solve: ");
+    number_of_conversions = scanf("%d%c%d", &left, &operation, &right);
+    clearKeyboardBuffer();
+    
+    while(number_of_conversions != 3 || ((operation == '/') && (right == 0))
+         || ((operation != '+') && (operation != '-') && (operation != '*') && (operation != '/'))) 
+    {
+        if((operation == '/') && (right == 0))
+        {
+            printf("Error, can't divide by zero.\nEnter in new equation: ");
+        }
+        else if((operation != '+') && (operation != '-') && (operation != '*') && (operation != '/'))
+        {
+            printf("Error, invalid operation. This calculator can only do +,-,*,/\n");
+            printf("Enter in a new equation: ");
+        }
+        else
+        {
+            printf("Error, enter in a new equation ex:a+b :");
+        }
+        number_of_conversions = scanf("%d%c%d", &left, &operation, &right);
+        clearKeyboardBuffer();
+    }
+    
+    printf("The result is %lf\n", eval_equation(left, operation, right, 0));
+}
+
+double eval_equation(double left, char operation, double right, int repetition)
+{
+    double result = 0.0;
+    
+    while(repetition--)
+    {
+        switch(operation)
+        {
+            case '+':
+            result = left + right;
+            break;
+            case '-':
+            result = left - right;
+            break;
+            case '*':
+            result = left * right;
+            break;
+            case '/':
+            result = left / right;
+            break;
+        }
+    }
+    return result;
 }
 
 bool askContinue()
@@ -36,7 +95,8 @@ bool askContinue()
         
         switch(y_or_n)
         {
-            case 'y': case 'Y':
+            case 'y': 
+            case 'Y':
                 wantContinue = TRUE;
                 break;
             case 'n':
@@ -58,6 +118,4 @@ void clearKeyboardBuffer()
     {
         scanf("%c", &temp);
     }while(temp != '\n');
-    
-    while(getchar() == '\n')
 }
