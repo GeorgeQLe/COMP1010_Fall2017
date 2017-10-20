2a91c941dce738c21bf12e98c6cd420379261c48
George Le <george_le@student.uml.edu>
2017-10-20 22:35:23 +0000

    Added statemachine to project GL

--- Readme.md
+++ Readme.md
@@ -1,4 +1,9 @@
-__COMP 1010__
+__COMP 1010 FALL 2017__
 
 This project is designed to teach computing 1 students on basic C syntax
 and style.
+
+Friday Recitation (10/20) - Beginning of AI project. Teach students about finite state
+                            machines with structs, functions, and enumerated types.
+                            
+Tuesday Recitation (10/24) - Creation of agent states, pointers, dot and arrow operator
--- test.c
+++ test.c
@@ -7,80 +7,36 @@
 #include <stdio.h> // printf(), scanf() 
 
 typedef enum Bool { UNSET = -1, FALSE, TRUE} bool;
+typedef enum skeleton_states{NULLSTATE = -1, EXIT = 0, IDLE = 1, DEFENDS, GLARES, DIES, skeletonATTACKS} SkeletonStates;
+typedef enum hero_actions {NOTHING = -1, FLEE, heroATTACKS, ADVANCES} HeroStates;
 
-void receives_input();
+// Function that prompts and receives input from the user on what equation they would like 
+// to be solved. Calls eval_equations() and then prints the result.
+void receives_equation_input();
 double eval_equation(double left, char operation, double right, int repetition);
+
+// Function that prompts and receives input from the user on whether they want to continue
+// using the program or not.
+// Returns TRUE if they want to continue, returns FALSE otherwise
 bool askContinue();
+
+// Self-contained function that clears the keyboard buffer using a brute force method
 void clearKeyboardBuffer();
+
+typedef struct statemachine {
+    SkeletonStates previous;
+    SkeletonStates current;
+    SkeletonStates next;
+} StateMachine;
 
 int main(void)
 {
     do
     {
-        receives_input();
+        // infinite loop
     } while(askContinue());
 }
 
-void receives_input()
-{
-    // receives two values from the user to solve in an equation
-    int left, right;
-    // receives the return value of scanf to validate input
-    int number_of_conversions;
-    // receives the user's desired operation
-    char operation;
-    
-    printf("Enter in an equation for this program to solve: ");
-    number_of_conversions = scanf("%d%c%d", &left, &operation, &right);
-    clearKeyboardBuffer();
-    
-    while(number_of_conversions != 3 || ((operation == '/') && (right == 0))
-         || ((operation != '+') && (operation != '-') && (operation != '*') && (operation != '/'))) 
-    {
-        if((operation == '/') && (right == 0))
-        {
-            printf("Error, can't divide by zero.\nEnter in new equation: ");
-        }
-        else if((operation != '+') && (operation != '-') && (operation != '*') && (operation != '/'))
-        {
-            printf("Error, invalid operation. This calculator can only do +,-,*,/\n");
-            printf("Enter in a new equation: ");
-        }
-        else
-        {
-            printf("Error, enter in a new equation ex:a+b :");
-        }
-        number_of_conversions = scanf("%d%c%d", &left, &operation, &right);
-        clearKeyboardBuffer();
-    }
-    
-    printf("The result is %lf\n", eval_equation(left, operation, right, 0));
-}
-
-double eval_equation(double left, char operation, double right, int repetition)
-{
-    double result = 0.0;
-    
-    while(repetition--)
-    {
-        switch(operation)
-        {
-            case '+':
-            result = left + right;
-            break;
-            case '-':
-            result = left - right;
-            break;
-            case '*':
-            result = left * right;
-            break;
-            case '/':
-            result = left / right;
-            break;
-        }
-    }
-    return result;
-}
 
 bool askContinue()
 {
@@ -118,4 +74,65 @@ void clearKeyboardBuffer()
     {
         scanf("%c", &temp);
     }while(temp != '\n');
+}
+
+void receives_equation_input()
+{
+    // receives two values from the user to solve in an equation
+    int left, right;
+    // receives the return value of scanf to validate input
+    int number_of_conversions;
+    // receives the user's desired operation
+    char operation;
+    
+    printf("Enter in an equation for this program to solve: ");
+    number_of_conversions = scanf("%d%c%d", &left, &operation, &right);
+    clearKeyboardBuffer();
+    
+    while(number_of_conversions != 3 || ((operation == '/') && (right == 0))
+         || ((operation != '+') && (operation != '-') && (operation != '*') && (operation != '/'))) 
+    {
+        if((operation == '/') && (right == 0))
+        {
+            printf("Error, can't divide by zero.\nEnter in new equation: ");
+        }
+        else if((operation != '+') && (operation != '-') && (operation != '*') && (operation != '/'))
+        {
+            printf("Error, invalid operation. This calculator can only do +,-,*,/\n");
+            printf("Enter in a new equation: ");
+        }
+        else
+        {
+            printf("Error, enter in a new equation ex:a+b : ");
+        }
+        number_of_conversions = scanf("%d%c%d", &left, &operation, &right);
+        clearKeyboardBuffer();
+    }
+    
+    printf("The result is %lf\n", eval_equation(left, operation, right, 0));
+}
+
+double eval_equation(double left, char operation, double right, int repetition)
+{
+    double result = 0.0;
+    
+    while(repetition--)
+    {
+        switch(operation)
+        {
+            case '+':
+            result = left + right;
+            break;
+            case '-':
+            result = left - right;
+            break;
+            case '*':
+            result = left * right;
+            break;
+            case '/':
+            result = left / right;
+            break;
+        }
+    }
+    return result;
 }
